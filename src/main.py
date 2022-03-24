5bfd1387768a4d23d9d84431a1f29fc42c5076c9
Ushikava <bryuhanov-aleksey@yandex.ru>
2022-03-24 23:08:55 +0800

    q
    q

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import cProfile
 
 from pprint import pprint as pp
+from turtle import left
 from logger import lg
 from logger import lg, initLogger
 
@@ -16,11 +17,6 @@ from drawing.draw import draw_island, draw_islands
 
 initLogger(lg.DEBUG)
 lg.info("Start")
-
-
-
-
-
 
 def check_cnt_top_side(island:Island, y:int, step_y:int) -> int:
   if island.top > y*step_y: return 0
@@ -72,8 +68,6 @@ def check_from_dir(island:Island, x:int, y:int, step_x:int, step_y:int):
 
   return dir_come_from
 
-
-
 def check_to_dir(island:Island, x:int, y:int, step_x:int, step_y:int):
   WALL_DIR = _get_dir_dictionary()
   dir_come_to = WALL_DIR['none']
@@ -99,51 +93,6 @@ def check_to_dir(island:Island, x:int, y:int, step_x:int, step_y:int):
   if dir_come_to == WALL_DIR['none']:
     dir_come_to = WALL_DIR['scum']
   return dir_come_to
-
-
-
-def first_column_fragments(islands:list[Island], x:int, y:int, step_x:int, step_y:int):
-  
-  if len(islands) == 0:
-    return False
-
-  wall_dir = {'left': 0, 'top': 1, 'right':2, 'down':3, 'scum':4}
-  success = False
-  dir_come_from = wall_dir['scum']
-  dir_come_to = wall_dir['scum']
-  #first_island = True
-
-  islands.sort(key=lambda x: (x.right - x.left), reverse=True)
-  #if len(islands[0]) >= step_x and islands[0].left == x*step_x and islands[0].right == (x+1)*step_x:
-  #  return True
-
-  #islands.sort(key=lambda x: x.right)
-
-  for single in islands:
-    
-    dir_come_from = check_from_dir(single,x,y,step_x,step_y)
-
-    if (dir_come_from == wall_dir['scum']) or (dir_come_from != dir_come_to and dir_come_to != wall_dir['scum']):
-      success = False
-      continue
-
-    dir_come_to = check_to_dir(single,x,y,step_x,step_y)
-
-    if dir_come_to == wall_dir['right']:
-      return True
-
-    if dir_come_from == wall_dir["left"] and dir_come_to == wall_dir["scum"]:
-      return False
-
-    if dir_come_to == wall_dir['scum']:
-      dir_come_from = wall_dir['scum']
-    else:
-      #first_island = False
-      success = True
-
-  return success  
-
-
 
 def middle_fragments(islands:list[Island], x:int, y:int, step_x:int, step_y:int):
   cell = Fragment_info()
@@ -195,7 +144,67 @@ def middle_fragments(islands:list[Island], x:int, y:int, step_x:int, step_y:int)
 
   return cell
 
+def last_column_fragments(islands:list[Island], x:int, y:int, step_x:int, step_y:int):
+  cell = Fragment_info()
+  island_len = 0
 
+  if len(islands) == 0:
+    return island_len
+
+  WALL_DIR = _get_dir_dictionary()
+
+  dir_come_from = WALL_DIR['scum']
+  dir_come_to = WALL_DIR['scum']
+
+  dir_couple = _get_fragment_dir_tuple()
+  #fr_to_array:list[dir_couple] = []
+
+  #islands.sort(key=lambda x: x.left)
+  islands.sort(key=lambda x: (x.right - x.left), reverse=True)
+
+  for single in islands:
+    dir_come_from = check_from_dir(single,x,y,step_x,step_y)
+ 
+    if dir_come_from == WALL_DIR["scum"]:
+      return island_len
+
+    if dir_come_from == WALL_DIR["left"] or dir_come_from == WALL_DIR["down"]:
+      if (single.width > 3):
+        dir_come_to = check_to_dir(islands[0],x,y,step_x,step_y)
+        if dir_come_to != WALL_DIR['scum']:
+          continue
+        else:
+          island_len = single.width
+          return island_len
+  
+  return island_len
+
+  # for single in islands:
+
+  #   dir_come_from = check_from_dir(single,x,y,step_x,step_y)
+
+  #   if (single.width > 3):
+  #     dir_come_to = check_to_dir(single,x,y,step_x,step_y)
+  #   else:
+  #     dir_come_to = WALL_DIR['scum']
+      
+  #   fr_to_array.append (
+  #     dir_couple(fr=dir_come_from, to=dir_come_to))
+
+  #   total_top    += check_cnt_top_side(single, y, step_y)
+  #   total_bottom += check_cnt_bottom_side(single, y, step_y)
+  # x = 10
+
+  # if all([i.fr==WALL_DIR['scum'] for i in fr_to_array]):
+  #   return cell
+
+  # if all([i.fr==WALL_DIR['scum'] for i in fr_to_array if i.to == WALL_DIR['right']]):
+  #   return cell
+
+  # if all([i.to==WALL_DIR['scum'] for i in fr_to_array if i.fr == WALL_DIR['left']]):
+  #   return cell
+
+  # return cell
 
 #|ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ|
 #|ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ|
@@ -206,7 +215,7 @@ def middle_fragments(islands:list[Island], x:int, y:int, step_x:int, step_y:int)
 if __name__ == "__main__":
   
   #fileName = "nameless.png"
-  fileName = "test1.png"
+  fileName = "test3.png"
 
   img:np.ndarray     = cv2.imread(PATH_TO_INPUT_ + fileName, cv2.IMREAD_GRAYSCALE)
   img_clr:np.ndarray = cv2.imread(PATH_TO_INPUT_ + fileName)
@@ -222,6 +231,8 @@ if __name__ == "__main__":
 
   ecg_cells:list[list[Fragment_info]] = []
   cells_saturation = []
+  islands_len = []
+  counting = 0
   index = 0
   sredn_arifm = 0
   
@@ -239,21 +250,10 @@ if __name__ == "__main__":
   WALL_DIR = _get_dir_dictionary()
 
   fragmentsWithIslands = [[] for x in range(img.shape[1] // step_x)]
-  sequence   = [x for x in range(img.shape[1] // step_x)]
+  sequence   = [x for x in range(1, img.shape[1] // step_x -1)]
   y_sequence = [y for y in range(img.shape[0] // step_y)]
-  x_sequence = []
+  x_sequence = sequence
 
-  mid = len(sequence) // 2
-  x_sequence.append(mid)
-  l1, l2 = mid, mid
-
-  while l1 > 0 and l2 < len(sequence):
-    if l1 > 0: 
-      l1 -= 1
-      x_sequence.append(l1)
-    if l2 < len(sequence)-1: 
-      l2 += 1
-      x_sequence.append(l2)
 
   cv2.imshow('w', img_isl)
   cv2.waitKey(10)
@@ -264,13 +264,10 @@ if __name__ == "__main__":
       bottom_line = 150
       upper_line = 255
       step = 3
+
       for up_value in range(bottom_line,upper_line,step):
-        
-        if x == 0 or x == count_of_ecg:
-          up_value = round(sredn_arifm)
 
         lg.debug(f">> step [{x}|{y}][{up_value}]")
-        # lg.debug(f">> step [{x}|{y}][{up_value}]")
 
         mask_inv = get_mask_from_gray(img, upper_val=up_value)      
         islandsInFragment = fragment_calculate(y*step_y,x*step_x,  step_y+1,step_x+1, mask_inv)
@@ -293,15 +290,8 @@ if __name__ == "__main__":
 
         one_of_works = False
         one_cell:Fragment_info
-
-        if x == 0:
-          one_of_works = True
-          #one_of_works = first_column_fragments(islandsInFragment,x,y,step_x,step_y)
-        elif x == count_of_ecg:
-          one_of_works = True
-        else:
-          one_cell = middle_fragments(islandsInFragment,x,y,step_x,step_y)
-          # one_of_works = len(one_cell) > 0
+        
+        one_cell = middle_fragments(islandsInFragment,x,y,step_x,step_y)
         
         if len(one_cell) == 0:
           continue
@@ -345,12 +335,101 @@ if __name__ == "__main__":
           if index == (count_of_ecg * (count_of_col-2)):
             index = (count_of_ecg * count_of_col)
             sredn_arifm /= (count_of_ecg * (count_of_col-2))
-
-          
-          
+            sredn_arifm += 5
+              
           fragmentsWithIslands[x].append(islandsInFragment.copy())
           cv2.imwrite(PATH_TO_OUTPUT_ + f"{x}_{y}.png", img_isl)
           break
+  
+  # x = count_of_ecg
+  
+  # for y in y_sequence:
+  #   bottom_line = 150
+  #   upper_line = 255
+  #   step = 3
+  #   islands_len.clear()
+   
+  #   bottom_line = round(sredn_arifm)
+  #   bottom_line -= step*5
+
+  #   for up_value in range(bottom_line,upper_line,step):
+
+  #     lg.debug(f">> step [{x}|{y}][{up_value}]")
+  #     # lg.debug(f">> step [{x}|{y}][{up_value}]")
+
+  #     mask_inv = get_mask_from_gray(img, upper_val=up_value)      
+  #     islandsInFragment = fragment_calculate(y*step_y,x*step_x,  step_y+1,step_x+1, mask_inv)
+
+  #     i=0
+  #     while i != len(islandsInFragment):
+  #       if islandsInFragment[i].width <= 3 and islandsInFragment[i].height <= 3:
+  #         del islandsInFragment[i]
+  #       else:
+  #         i+=1
+
+  #     #------------------------------------------------------
+  #     img_isl[y*step_y:(y+1)*step_y, x*step_x:(x+1)*step_x] = 255
+  #     img_isl = cv2.rectangle(img_isl, (x*step_x, y*step_y),((x+1)*step_x,(y+1)*step_y,), color=(0,0,255))
+  #     img_isl = draw_islands(islandsInFragment, img_isl)
+  #     cv2.imshow('w', img_isl)
+  #     cv2.waitKey(10)
+  #     #------------------------------------------------------
+
+
+  #     one_of_works = False
+  #     one_cell:Fragment_info
+  #     len_of_island = 0
+
+  #     len_of_island = last_column_fragments(islandsInFragment,x,y,step_x,step_y)      
+
+  #     if len(islands_len) < 5:
+  #       islands_len.append(len_of_island)
+  #       continue
+    
+  #     summ1 = sum(islands_len) - islands_len[-1]
+  #     summ2 = sum(islands_len) - islands_len.pop(0)
+      
+  #     summ1 = summ1/4
+  #     summ2 = summ2/4
+  #     # didn't work
+  #     if (summ2 >= summ1 and summ2 <= summ1 + 5):
+  #       continue
+
+  #     upper_cell_trash = 0
+  #     cur_cell_trash = 0
+
+  #     if y == 0:
+  #       one_of_works = True
+  #     else:
+  #       for direct in ecg_cells[y-1][x]:
+  #         if direct.fr == WALL_DIR['down'] and direct.to == WALL_DIR['scum']:
+  #           upper_cell_trash += 1
+  #         if direct.to == WALL_DIR['down'] and direct.fr == WALL_DIR['scum']:
+  #           upper_cell_trash += 1
+        
+  #       expecting_top_crossing = ecg_cells[y-1][x].cnt_bottom_intersection - upper_cell_trash
+
+  #       for direct in one_cell:
+  #         if direct.fr == WALL_DIR['top'] and direct.to == WALL_DIR['scum']: 
+  #           cur_cell_trash += 1
+  #         if direct.to == WALL_DIR['top'] and direct.fr == WALL_DIR['scum']:
+  #           cur_cell_trash += 1
+            
+  #       actually_top_crossing = one_cell.cnt_top_intersection - cur_cell_trash
+
+  #       if expecting_top_crossing == actually_top_crossing:
+  #         one_of_works = True
+
+
+  #     if one_of_works == True:
+  #       lg.debug(f"При насыщенности {up_value} островов найдено {len(islandsInFragment)}")
+
+  #       one_cell.saturation = up_value
+  #       ecg_cells[y][x] = one_cell
+
+  #       fragmentsWithIslands[x].append(islandsInFragment.copy())
+  #       cv2.imwrite(PATH_TO_OUTPUT_ + f"{x}_{y}.png", img_isl)
+  #       break
 
         
 
